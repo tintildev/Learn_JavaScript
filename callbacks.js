eea231cf992c1c20296798bb9088c842d1f4aebe
Gerak842 <34619286+Gerak842@users.noreply.github.com>
2019-09-11 05:00:53 +0200

    Callbacks
    Intervall, Eventlistener

---
+++ callbacks.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Zähler der jede Sekunde um 1 hoch zählt
+let counter = 0;
+setInterval(function(){
+    counter = counter + 1;
+    console.log(counter);
+},1000)
+
+//Eventlistener, gib eine Ausgabe wenn ein Button geklickt wurde.
+window.addEventListene("load", function(){
+    document.getElementById("click-me").addEventListener("click", function(){
+        console.log("Button wurde gedrückt");
+    
+    }, false);
+})
