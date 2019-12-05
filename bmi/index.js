8c45c0fb7f6c99eb6e959a4ffc1f670321361e74
martinklestil <Martin.klestil@gmx.at>
2019-12-05 08:17:36 +0100

    BMI
    BMI - Berechnung bei Eingabe

--- bmi/index.js
+++ bmi/index.js
@@ -6,20 +6,34 @@ window.addEventListener("load", function() {
 
     let button = document.getElementById("calculate");
 
-    
-    button.addEventListener("click", function(){
-        let heightObj = document.getElementById("height");
-        let weightObj = document.getElementById("weight");
+    let heightObj = document.getElementById("height");
+    let weightObj = document.getElementById("weight");
 
+    // Grundlegende Funktion
+    let calculateBmi = function(){
         let height = parseFloat(heightObj.value.replace(",", "."));
         let weight = parseFloat(weightObj.value);
 
         let bmi = (weight / (height * height));
 
-        alert("Dein BMI ist: " + bmi);
+        //Ausgabe:
+        // alert("Dein BMI ist: " + bmi);
         let bmiValue = document.getElementById("bmi");
-        bmiValue.appendChild(document.createTextNode("Dein BMI ist: " + bmi));
-        });
+        bmiValue.innerText = "Dein BMI ist: " + bmi;
+
+    }
+
+    // Aktion bei Klick
+    button.addEventListener("click", calculateBmi);
+
+    //Aktion bei Veränderung (Feld verlassen)
+    heightObj.addEventListener("change", calculateBmi);
+    weightObj.addEventListener("change", calculateBmi);
+
+    //Aktion bei Veränderung (direkt bei Eingabe)
+    heightObj.addEventListener("keyup", calculateBmi);
+    weightObj.addEventListener("keyup", calculateBmi);
+
     });
 
 
