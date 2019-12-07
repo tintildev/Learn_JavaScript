ab478f63aa674136a0105167785687162e011a16
martinklestil <Martin.klestil@gmx.at>
2019-12-07 13:01:09 +0100

    work with arrays
    work with arrays

--- arrays/index.js
+++ arrays/index.js
@@ -10,3 +10,21 @@ console.log(persons);
 // New person
 persons.push("Paula");
 console.log(persons);
+
+//Große von Array
+console.log(persons.length);
+
+//Zugreifen
+console.log(persons[0]);
+
+//Element am Anfang hinzufügen
+persons.unshift("Andrew");
+
+//Elemente entfernen (am Ende)
+persons.pop();
+
+//Element am anfang entfernen
+persons.shift();
+
+//Bei Schift wird das Element entfernt aus dem Array, kann aber in einer Variable gespeichert werden
+let removePerson = persons.shift();
