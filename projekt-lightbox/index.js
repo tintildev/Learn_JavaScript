4f009b45336cf08dafcd21edda13d4924431b79a
martinklestil <Martin.klestil@gmx.at>
2019-12-18 09:41:44 +0100

    projekt lightbox
    Abdunkeln beim klick

--- projekt-lightbox/index.js
+++ projekt-lightbox/index.js
@@ -4,5 +4,8 @@ jQuery(document).ready(function($){
     $(".gallery-item").click(function(event){
         //Link folgen deaktiviert
         event.preventDefault();
+
+        let overlay = $.parseHTML("<div id='overlay'></div>");
+        $("body").append(overlay);
     });
 });
