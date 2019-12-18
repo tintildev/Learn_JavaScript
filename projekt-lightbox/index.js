2f43851653d58b1b3f8a7478d8092e2cb6b884c0
martinklestil <Martin.klestil@gmx.at>
2019-12-18 09:57:50 +0100

    projekt-lightbox
    img im overlay

--- projekt-lightbox/index.js
+++ projekt-lightbox/index.js
@@ -7,5 +7,10 @@ jQuery(document).ready(function($){
 
         let overlay = $.parseHTML("<div id='overlay'></div>");
         $("body").append(overlay);
+
+        let image = $.parseHTML("<img></img>");
+        let src = $(this).attr("href");
+        $(image).attr("src", src);
+        $("body").append(image);
     });
 });
