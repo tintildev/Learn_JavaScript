f0cd908ab5a60dbc4c736013bca2c07545660608
martinklestil <Martin.klestil@gmx.at>
2019-12-19 21:45:28 +0100

    projekt-lightbox schließen
    Schließen des Bildes

--- projekt-lightbox/index.js
+++ projekt-lightbox/index.js
@@ -8,16 +8,26 @@ jQuery(document).ready(function($){
         let overlay = $.parseHTML("<div id='overlay'></div>");
         $("body").append(overlay);
 
+        //Div mit CSS
         let image = $.parseHTML("<img id='overlay-image'></img>");
         let src = $(this).attr("href");
         $(image).attr("src", src);
+        /*
         $(image).click(function(){
             $("#overlay").remove();
             $("overlay-container").remove();
 
         });
+        */
 
         let conatiner = $.parseHTML("<div id='overlay-container'></div>");
+
+        //Schlies mechanismus
+        $(conatiner).click(function(){
+            $("#overlay").remove();
+            $("#overlay-container").remove();
+
+        });
         $(conatiner).append(image);
 
         $("body").append(conatiner);
