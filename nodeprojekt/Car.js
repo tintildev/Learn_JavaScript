7f0b983b8611b20402eead78c8ef792361709968
martinklestil <Martin.klestil@gmx.at>
2019-12-22 20:51:26 +0100

    node.js mehrere Dateien
    Arbeiten mit require und exports

---
+++ nodeprojekt/Car.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Car {
+    drive(){
+        console.log("drive()!");
+    }
+}
+
+//Zum Expotieren
+module.exports = Car;
--- nodeprojekt/app.js
+++ nodeprojekt/app.js
@@ -1,5 +1,12 @@
 "use strict";
 
+//Zugriff auf Car
+require("./Car.js");
+
 for(let x = 0; x <= 10; x++){
     console.log(x);
 }
+
+//Jetzt können wir Car nutzen
+let a = new Car();
+a.drive;
