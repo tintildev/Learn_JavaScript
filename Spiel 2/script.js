e623c57d6b022cbc83ad6acf1da6ac4711eeca68
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2020-08-16 11:41:10 +0200

    Add Spiel 2
    Build a small jump game.

---
+++ Spiel 2/script.js
@@ -0,0 +1,25 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+//Beim Klicken Animation hinzufügen und nach 500ms löschen.
+function jump(){
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    
+    setTimeout(function(){
+        character.classList.remove("animate");
+    }, 500);
+
+}
+//Alle 10 Sekunden Checken wo die Position ist.
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+        block.style.animation ="none";
+        block.style.display ="none";
+        alert("Verloren");
+    }
+},10)
