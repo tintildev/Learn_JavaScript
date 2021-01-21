d4b24407ca789f936564da37220f6ff262e910da
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-01-21 11:20:35 +0100

    add iframe

--- remove_div/remove.js
+++ remove_div/remove.js
@@ -1,8 +1,21 @@
 "use strict";
 
 window.onload = function() {
-    var element = document.getElementById("test");
-    element.remove();
-    console.log("Wurde gelöscht!")
+    let kontroll = document.getElementById("test1");
+    if (kontroll){
+        var element = document.getElementById("test");
+        element.remove();
+        console.log("Wurde gelöscht!")
+    }else {
+        let newDiv = document.createElement("div");
+        let iframe = document.createElement("iframe");
+        iframe.style.display= "none";
+        iframe.src = "https://www.youtube.com/embed/J9s2J0P5nWo";
+        iframe.frameborder = 0;
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        newDiv.appendChild(iframe);
+        document.getElementById("container").appendChild(newDiv);
+    }
 }
+
 
