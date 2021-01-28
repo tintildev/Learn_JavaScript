486304ca45ee0f96b1f6087647a1269c1afa0102
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-01-28 09:49:35 +0100

    add remove and crossed out function

--- todolist/main.js
+++ todolist/main.js
@@ -2,9 +2,21 @@ let addToDoButton = document.getElementById('addButton');
 let toDoContainer = document.getElementById('toDoList');
 let myInputField = document.getElementById('inputField');
 
+//add function
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.className = 'paragraph';
     paragraph.innerText = myInputField.value;
     toDoContainer.appendChild(paragraph);
+
+    //crossed out 
+    paragraph.addEventListener('click', function(){
+        this.innerHTML = '<del>'+ this.innerHTML + '</del>';
+        //remove
+        this.addEventListener('click', function(){
+            this.parentNode.removeChild(this);
+        })
+    })
 })
+
+
