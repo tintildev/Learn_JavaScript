dcf68463c711fcb8dd670f3c595913a36bc15ee1
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-02-03 21:36:30 +0100

    start new project expanse

---
+++ expanse/main.js
@@ -0,0 +1,23 @@
+let addToDoButton = document.getElementById('addButton');
+let toDoContainer = document.getElementById('expanseList');
+let myInputField = document.getElementById('inputField');
+
+//add function
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.className = 'paragraph';
+    paragraph.innerText = myInputField.value;
+    toDoContainer.appendChild(paragraph);
+    myInputField.value ="";
+
+    //crossed out 
+    paragraph.addEventListener('click', function(){
+        this.innerHTML = '<del>'+ this.innerHTML + '</del>';
+        //remove
+        this.addEventListener('click', function(){
+            this.parentNode.removeChild(this);
+        })
+    })
+})
+
+
