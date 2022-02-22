18d8fe3be9b457fc2a18213e01879adacfcb2d16
martinklestil <Martin.klestil@gmx.at>
2022-02-22 16:32:48 +0100

    update structure

--- vowel_counter/counter.js
+++ vowel_counter/counter.js
@@ -2,11 +2,13 @@
 
 let inputText = document.getElementById('inputText');
 let inputForm = document.getElementById('inputForm');
+let result = document.getElementById('result');
 console.log("Start");
 inputForm.addEventListener('submit', function(){
     console.log("Start Function");
     var text = inputText.value;
     var temp = text.split(" ");
-    alert("There were " + temp.length + " Words found.")
+    //alert("There were " + temp.length + " Words found.")
+    result.textContent = "There were " + temp.length + " Words found.";
     inputText.value ="";
 })
