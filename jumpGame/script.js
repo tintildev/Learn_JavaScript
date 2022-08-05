3e4ff86d41362d66c63f35a222906d29d912369d
martinklestil <Martin.klestil@gmx.at>
2022-08-05 17:22:47 +0200

    update projects and jumpGame

--- jumpGame/script.js
+++ jumpGame/script.js
@@ -1,25 +1,45 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let jumpBtn = document.getElementById("jump");
+let startBtn = document.getElementById("start");
+
 
 //Beim Klicken Animation hinzufügen und nach 500ms löschen.
-function jump(){
+let jump = function(){
     if(character.classList != "animate"){
         character.classList.add("animate");
     }
-    
     setTimeout(function(){
         character.classList.remove("animate");
     }, 500);
 
 }
 //Alle 10 Sekunden Checken wo die Position ist.
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+let checkDead = setInterval(function(){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
     if(blockLeft < 20 && blockLeft > 0 && characterTop >= 100){
-        block.style.animation ="none";
-        block.style.display ="none";
+        block.classList.remove("block-animate");
+
         alert("Verloren");
     }
 },10)
+
+//Space
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space'){
+        jump();
+    }
+});
+
+//Start
+startBtn.addEventListener("click",function(){
+    jumpBtn.addEventListener("click", jump);
+    block.classList.add("block-animate");
+})
+
+
+    
+
+
